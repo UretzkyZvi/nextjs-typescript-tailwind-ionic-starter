88f60b12159f4b553a01bae1ab78e3a826b38679
greguretzky <uretsky.zvi@gmail.com>
2024-02-18 11:44:08 +0100

    Update next.config.mjs with rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '',
-    transpilePackages: ['@ionic/react', '@ionic/core',  'ionicons'],
-    output:'export',   
+  basePath: "",
+  transpilePackages: ["@ionic/react", "@ionic/core", "ionicons"],
+  output: "export",
+  rewrites: () => {
+    return [
+      {
+        source: "/tabs/:path*",
+        destination: "/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
